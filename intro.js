a5bae19e6c8a536b75ffeb932073171117eff5b1
Jayden Banks <jayden.banks21@gmail.com>
2021-07-12 10:36:36 -0600

    finished demo

---
+++ intro.js
@@ -0,0 +1,13 @@
+// console.log is a prebuilt function that prints a value to the console
+console.log("hello,world!");
+
+const firstName = "Jayden";
+let age = "27";
+
+console.log(firstName, age);
+
+// Numbers
+let x = 4;
+let favNum = 12;
+let favSqr = 12 * 12;
+console.log(favNum / x);
